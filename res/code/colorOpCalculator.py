427179847ed46c5bb21d7bb95f77fc0e3167920a
Moenupa <moenupa@outlook.com>
2021-01-07 16:41:55 +0800

    🕊 Pigeon Light Update

--- res/code/colorOpCalculator.py
+++ res/code/colorOpCalculator.py
@@ -1,55 +1,60 @@
-class opacityCalculator():
-    @staticmethod
-    def hex_to_int(hex_num: str) -> int:
-        return int(hex_num, base = 16)
+def hex_to_int(hex_num: str) -> int:
+    return int(hex_num, base = 16)
 
-    @staticmethod
-    def hex_to_rgb(RGB: str) -> list:
-        RGB = RGB.replace("#", "")
-        return [hex_to_int(RGB[i,i+2]) for i in range(0,5,2)]
+def hex_to_rgb(rgb: str) -> list:
+    rgb = rgb.replace("#", "")
+    return [hex_to_int(rgb[i:(i+2)]) for i in range(0,5,2)]
 
-    @staticmethod
-    def rgb_to_hex(arr_RGB: list) -> str:
-        prefix = "#"
-        return prefix + ''.join(hex(i)[-2:] for i in arr_RGB).upper()
+def rgb_to_hex(arr_RGB: list) -> str:
+    prefix = "#"
+    return prefix + ''.join(hex(i)[-2:] for i in arr_RGB).upper()
 
-    @staticmethod
-    def floatOpCal(opacity: float, overlay: str, background: str) -> str:
-        '''
-        Calculate cover color with color transparency
-        cover color = opacity * overlay + (1 - opacity) * background
-        @param overlay, background: "#RRGGBB" or "RRGGBB"
-        @param opacity: float of 0~1
-        '''
-        overlay_hex = hex_to_rgb(overlay)
-        background_hex = hex_to_rgb(background)
-        
-        cover_color = [
-            int(opacity * overlay_hex[i] + (1 - opacity) * background_hex[i])
-            for i in range(3)
-        ]
+def floatOpCal(opacity: float, overlay: str, background: str) -> str:
+    '''
+    Calculate cover color with color transparency
+    ``cover color = opacity * overlay + (1 - opacity) * background``
 
-        return rgb_to_hex(cover_color)
+    Args:
+        ``overlay`` and ``background``: in format ``#RRGGBBTT`` or ``RRGGBBTT``
+        `opacity`: float, 0~1
+    
+    Returns:
+        string ""
+    '''
+    overlay_hex = hex_to_rgb(overlay)
+    background_hex = hex_to_rgb(background)
+    
+    cover_color = [
+        int(opacity * overlay_hex[i] + (1 - opacity) * background_hex[i])
+        for i in range(3)
+    ]
 
-    @staticmethod
-    def hexOpCal(overlay: str, background: str) -> str:
-        '''
-        Calculate cover color with color transparency
-        cover color = opacity * overlay + (1 - opacity) * background
-        @param overlay: "#RRGGBBTT" or "RRGGBBTT"
-        @param background: "#RRGGBB" or "RRGGBB"
-        '''
-        opacity = hex_to_int(overlay[-2:])/256.0
+    return rgb_to_hex(cover_color)
 
-        overlay_hex = hex_to_rgb(overlay)
-        background_hex = hex_to_rgb(background)
-        
-        cover_color = [
-            int(opacity * overlay_hex[i] + (1 - opacity) * background_hex[i])
-            for i in range(3)
-        ]
+def hexOpCal(overlay: str, background: str, flag: bool) -> str:
+    '''
+    Calculate cover color with color transparency
 
-        return rgb_to_hex(cover_color)
+    formula ``cover color = opacity * overlay + (1 - opacity) * background``
+
+    Args:
+        ``overlay`` and ``background``: in format #RRGGBBTT or RRGGBBTT;
+    
+    Returns:
+        string ""
+    '''
+
+    opacity = hex_to_int(overlay[-2:])/256.0
+
+    overlay_hex = hex_to_rgb(overlay)
+    background_hex = hex_to_rgb(background)
+    
+    cover_color = [
+        int(opacity * overlay_hex[i] + (1 - opacity) * background_hex[i])
+        for i in range(3)
+    ]
+
+    return rgb_to_hex(cover_color)
 
 if __name__ == "__main__":
     PigeonDark = {
@@ -75,7 +80,35 @@ if __name__ == "__main__":
         "darkPink"   : "#754260", "pink"   : "#CC66A3", "brightPink"   : "#FF80CC"
     }
 
-    print(opacityCalculator.hexOpCal(PigeonDark["blue"]+"3E", PigeonDark["black"]))
+    PigeonLight = {
+        "name": "Pigeon Light",
+        "author": "Moenupa",
+        
+        "foreground": "#1E1E1E",
+        "background": "#FFFFFF",
+        
+        "currentLineBackground": "EFEFEF",
+        "selectionBackground": "#D4D4D4",
+        
+        "cursorColor": "#000000",
+        
+        "darkBlack"  : "#000000", "black"  : "#1E1E1E", "brightBlack"  : "#3E3E3E",
+        "darkGray"   : "#666666", "gray"   : "#999999", "brightGray"   : "#A6A6A6",
+        "darkWhite"  : "#BBBBBB", "white"  : "#D4D4D4", "brightWhite"  : "#EFEFEF",
+
+        "darkRed"    : "#754242", "red"    : "#E64B43", "brightRed"    : "#FF3B30",
+        "darkYellow" : "#755642", "yellow" : "#E6BC17", "brightYellow" : "#FFCC00",
+        "darkOrange" : "#756B42", "orange" : "#E68F17", "brightOrange" : "#FF9500",
+        "darkGreen"  : "#567542", "green"  : "#3EAD5A", "brightGreen"  : "#34C759",
+        "darkCyan"   : "#427560", "cyan"   : "#67BAE0", "brightCyan"   : "#5AC8FA",
+        "darkBlue"   : "#426075", "blue"   : "#177BE6", "brightBlue"   : "#007AFF",
+        "darkMagenta": "#6B4275", "magenta": "#A25CC4", "brightMagenta": "#AF52DE",
+        "darkPink"   : "#754260", "pink"   : "#E6405F", "brightPink"   : "#FF2D55"
+    }
+
+    for color in ["red", "yellow", "orange", "green", "cyan", "blue", "magenta", "pink"]:
+        print("rgb-{0}: {1}".format(color, ",".join(str(i) for i in hex_to_rgb(PigeonDark[color]))))
+        #print(floatOpCal(0.5, PigeonLight[color], "#FFFFFF"))
         
 
     # origin "#155221"
